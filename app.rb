95829485fcd2f2a7b22c6fd198d5c5bbbbf8e376
JP Hastings-Spital <jphastings@gmail.com>
2010-07-15 16:39:37 +0100

    Now with more MAN

--- app.rb
+++ app.rb
@@ -1,18 +1,25 @@
 require 'rubygems'
 require 'sinatra'
 require 'haml'
+require 'yaml'
 
 get '/' do
-  haml :home
+  haml :home, :locals => YAML::load(open('audio/options.yaml'))
 end
 
 get '/message' do  
   content_type :mp3
+  response['Content-Disposition'] = 'attachment; filename=AnswerPhoneMessage.mp3'
   
-  out = ""
-  ["intro",params[:number].gsub(/[^0-9\ ]/," ").gsub(/\ +/," ")[0..32].strip.split(//),"mid",params[:reason] || 'c',"exit"].flatten.each do |clip|
-    out << open("audio/#{clip}.mp3").read
+  params[:intro]  = 'fintro1' if not File.exists?("audio/#{params[:intro]}.mp3")
+  params[:mid]    = 'fmid1'   if not File.exists?("audio/#{params[:mid]}.mp3")
+  params[:reason] = 'n2'      if not File.exists?("audio/#{params[:reason]}.mp3")
+  params[:exit]   = 'fexit1'  if not File.exists?("audio/#{params[:exit]}.mp3")
+  
+  sequence = [params[:intro],params[:number].gsub(/[^0-9\ ]/," ").gsub(/\ +/," ")[0..32].strip.split(//),params[:mid],params[:reason],params[:exit]].flatten
+  
+  sequence.each do |clip|
+    response.write open("audio/#{clip}.mp3").read
   end
-  #response.finish
-  out
+  response.finish
 end
---
+++ public/assets/site.js
@@ -0,0 +1,36 @@
+$(document).ready(function() {
+	$('#number').keydown(function(e) {
+		if ((e.keyCode < 48 || e.keyCode > 57) && (e.keyCode != 32 && e.keyCode != 189 && e.keyCode != 8)) {
+			e.preventDefault()
+		}
+	})
+	
+	$('form a').click(function(e) {
+		e.preventDefault()
+		$(this).fadeOut('fast',function() {
+			var next = $(this).next()
+			if (next.attr('href') == undefined) {
+				next = $(this).siblings().first()
+			}
+			next.fadeIn()
+			
+			$(this).parent().children('input').attr('value',next.attr('href').substr(1))
+			
+			$('form a').removeClass('wrongsex')
+			switch(next.attr('class')) {
+				case 'male':
+					$('form li[class="female"]').fadeOut()
+					$('form li[class="male"]').fadeIn()
+					$('form a[class="female"]').addClass('wrongsex')
+					break;
+				case 'female':
+					$('form li[class="female"]').fadeIn()
+					$('form li[class="male"]').fadeOut()
+					$('form a[class="male"]').addClass('wrongsex')
+					break;
+				case 'neutral':
+					$('form li').fadeIn()
+			}
+		})
+	})
+})
