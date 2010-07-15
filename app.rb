d349277274a6990e67ff21f5e4fe0fcb3fa96c9d
JP Hastings-Spital <jphastings@gmail.com>
2010-07-15 04:38:55 +0100

    Copy tweak & safety against crazy long numbers

--- app.rb
+++ app.rb
@@ -10,7 +10,7 @@ get '/message' do
   content_type :mp3
   
   out = ""
-  ["intro",params[:number].gsub(/[^0-9\ ]/," ").gsub(/\ +/," ").strip.split(//),"mid",params[:reason] || 'c',"exit"].flatten.each do |clip|
+  ["intro",params[:number].gsub(/[^0-9\ ]/," ").gsub(/\ +/," ")[0..32].strip.split(//),"mid",params[:reason] || 'c',"exit"].flatten.each do |clip|
     out << open("audio/#{clip}.mp3").read
   end
   #response.finish
